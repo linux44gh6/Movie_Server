1efa1894b7cc855a39725f566ff34b1f62c4d6a8
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-30 00:34:51 +0600

    done login user

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -6,11 +6,15 @@ import { AuthServices } from "./auth.services";
 const loginUser = catchAsync(async (req, res, next) => {
     const result = await AuthServices.loginUser(req.body)
 
+    const { refreshToken, accessToken } = result
+
+    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true })
+
     sendResponse(res, {
         statuscode: httpStatus.OK,
         success: true,
         message: "User login successfully",
-        data: result
+        data: { accessToken }
     })
 })
 
