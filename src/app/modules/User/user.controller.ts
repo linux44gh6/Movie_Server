795b00caa7f977d52f0e582dfd48de3622f9b0dc
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-29 23:05:59 +0600

    validation schema done for create user

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { UserServices } from "./user.services";
-
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const result = await UserServices.createUser(req.body)
 
-    res.status(200).json({
-        statusCode: 200,
+    sendResponse(res, {
+        statuscode: httpStatus.OK,
         success: true,
         message: 'User create successfully',
         data: result
---
+++ src/app/modules/User/user.validation.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+
+const create = z.object({
+    name: z.string({
+        required_error: "Name is required"
+    }),
+    email: z.string({
+        required_error: "Email is required"
+    }),
+    password: z.string({
+        required_error: "Password is required"
+    }),
+})
+
+
+export const UserValidationSchema = {
+    create
+}
