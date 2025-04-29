42a21efa36b630c692e6aab3c462f80706863105
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-30 00:05:43 +0600

    create user and pash password done

--- src/app/modules/User/user.services.ts
+++ src/app/modules/User/user.services.ts
@@ -1,7 +1,34 @@
+import prisma from "../../../helpers/prisma"
+import bcrypt from 'bcrypt'
 
 
 const createUser = async (payload: any) => {
 
+    const existingUser = await prisma.users.findFirst({
+        where: {
+            email: payload.email
+        }
+    })
+    if (existingUser) {
+        throw new Error("User with this email already exists");
+    }
+
+    const hashPassword: string = await bcrypt.hash(payload.password, 12)
+    const result = await prisma.users.create({
+        data: {
+            ...payload,
+            password: hashPassword
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            createAt: true,
+            updateAt: true
+        }
+    })
+
+    return result
 }
 
 export const UserServices = {
