3ed0377fa97f9e0bc8ae9b7dede0a7664ad250a4
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-30 21:02:12 +0600

    like video done

--- src/app/modules/Likes/likes.services.ts
+++ src/app/modules/Likes/likes.services.ts
@@ -4,71 +4,49 @@ import { IAuthUser } from "../../interface/common";
 
 
 const likeVideo = async (user: IAuthUser, payload: any) => {
+    
+    const result = await prisma.$transaction(async (tx) => {
+        const userData = await tx.user.findFirstOrThrow({
+            where: {
+                email: user?.email,
+                isDeleted: false
+            }
+        });
 
-    const userData = await prisma.user.findFirstOrThrow({
-        where: {
-            email: user?.email,
-            isDeleted: false
+        const existingVideo = await tx.video.findUnique({
+            where: {
+                id: payload.videoId
+            }
+        });
+
+        if (!existingVideo) {
+            throw new ApiError(404, 'Video not found');
         }
-    })
 
+        const existingLike = await tx.like.findFirst({
+            where: {
+                userId: userData.id,
+                videoId: existingVideo.id
+            }
+        });
 
-    const existingVideo = await prisma.video.findUnique({
-        where: {
-            id: payload.videoId
+        if (existingLike) {
+            return {
+                message: 'You have already liked this video'
+            };
         }
+
+        return await tx.like.create({
+            data: {
+                userId: userData.id,
+                videoId: existingVideo.id
+            }
+        });
     });
 
-    if (!existingVideo) {
-        throw new ApiError(404, 'Video not found')
-    }
+    return result;
+};
 
-    const existingLike = await prisma.like.findFirst({
-        where: {
-            userId: userData.id,
-            videoId: payload.videoId
-        }
-    })
-
-    if (existingLike) {
-        return {
-            message: 'You have already liked this video'
-        }
-    }
-
-    const result = await prisma.like.create({
-        data: {
-            userId: userData.id,
-            videoId: existingVideo.id
-        }
-    })
-
-    // const result = await prisma.$transaction(async (tx) => {
-    //     await tx.like.create({
-    //         data: {
-    //             userId: userData.id,
-    //             videoId: payload.videoId
-    //         }
-    //     })
-
-    //     await tx.video.update({
-    //         where: {
-    //             id: payload.videoId
-    //         },
-    //         data: {
-    //             like: {
-    //                 increment: 1
-    //             }
-    //         }
-    //     })
-
-    //     return {
-    //         message: "Video liked successfully"
-    //     }
-    // })
-    return result
-
-}
 
 export const LikeServices = {
     likeVideo
