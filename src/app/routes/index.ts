4e4f971a962dd093bc04415bb28b611447a7608d
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-30 08:00:50 +0600

    routes setup done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,11 @@ import express from 'express'
 import { UserRoutes } from '../modules/User/user.routes'
 import { AuthRouters } from '../modules/Auth/auth.routes'
 import { ContentRouter } from '../modules/Content/content.routes'
+import { CommentsRoutes } from '../modules/Comments/comments.rotes'
+import { LikesRoutes } from '../modules/Likes/likes.routes'
+import { PurchasesRoutes } from '../modules/Purchases/purchases.routes'
+import { ReviewRoutes } from '../modules/Reviews/reviews.routes'
+import { WatchListRoutes } from '../modules/WatchList/watchList.routes'
 
 const router = express.Router()
 
@@ -16,9 +21,29 @@ const modulesRoute = [
         route: AuthRouters
     },
     {
-        path:"/content",
-        route:ContentRouter
-    }
+        path: "/content",
+        route: ContentRouter
+    },
+    {
+        path: "/comments",
+        route: CommentsRoutes
+    },
+    {
+        path: "/likes",
+        route: LikesRoutes
+    },
+    {
+        path: "/purchases",
+        route: PurchasesRoutes
+    },
+    {
+        path: "/reviews",
+        route: ReviewRoutes
+    },
+    {
+        path: "/watch-list",
+        route: WatchListRoutes
+    },
 ]
 
 
